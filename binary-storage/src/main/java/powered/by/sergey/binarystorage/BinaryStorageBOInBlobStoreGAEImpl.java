e0cdb44f1006c23a66ed11f7ce77339ebfbd8ba3
Laesod <laesod@gmail.com>
2014-06-24 15:58:23 -0400

    Initial commit

---
+++ binary-storage/src/main/java/powered/by/sergey/binarystorage/BinaryStorageBOInBlobStoreGAEImpl.java
@@ -0,0 +1,120 @@
+package powered.by.sergey.binarystorage;
+
+/*
+ * #%L
+ * ProjectX2013_03_23_web
+ * %%
+ * Copyright (C) 2013 - 2014 Powered by Sergey
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class BinaryStorageBOInBlobStoreGAEImpl implements BinaryStorageBO {
+	private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+	
+	@Override
+	public byte[] getFile(String id) {
+	    BlobKey blobKey = new BlobKey(id);
+		return readBlobFully(blobKey);
+	}
+
+	@Override
+	public String saveFile(InputStream inputStream, HttpServletRequest req) {
+	    
+		Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+	    if (blobs.keySet().isEmpty()) { 
+	    	throw new RuntimeException("No File found"); 
+	    }
+
+	    Iterator<String> names = blobs.keySet().iterator();
+	    String blobName = names.next();
+	    BlobKey blobKey = blobs.get(blobName);
+	    /*	    BlobInfoFactory blobInfoFactory = new BlobInfoFactory();
+	    BlobInfo blobInfo = blobInfoFactory.loadBlobInfo(blobKey);
+
+	    String contentType = blobInfo.getContentType();
+	    long size = blobInfo.getSize();
+	    Date creation = blobInfo.getCreation();
+	    String fileName = blobInfo.getFilename();
+
+	    String title = req.getParameter("title");
+	    String description = req.getParameter("description");
+	    boolean isShared = "public".equalsIgnoreCase(req.getParameter("share"));
+*/	    
+		return blobKey.getKeyString();
+	}
+
+	@Override
+	public void deleteFile(String id) {
+	}
+	
+	public static byte[] readBlobFully(BlobKey blobKey) {
+
+		BlobstoreService blobstoreService = BlobstoreServiceFactory
+				.getBlobstoreService();
+		BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+
+		if (blobInfo == null)
+			return null;
+
+		if (blobInfo.getSize() > Integer.MAX_VALUE)
+			throw new RuntimeException(
+					"This method can only process blobs up to "
+							+ Integer.MAX_VALUE + " bytes");
+
+		int blobSize = (int) blobInfo.getSize();
+		int chunks = (int) Math
+				.ceil(((double) blobSize / BlobstoreService.MAX_BLOB_FETCH_SIZE));
+		int totalBytesRead = 0;
+		int startPointer = 0;
+		int endPointer;
+		byte[] blobBytes = new byte[blobSize];
+
+		for (int i = 0; i < chunks; i++) {
+
+			endPointer = Math.min(blobSize - 1, startPointer
+					+ BlobstoreService.MAX_BLOB_FETCH_SIZE - 1);
+
+			byte[] bytes = blobstoreService.fetchData(blobKey, startPointer,
+					endPointer);
+
+			for (int j = 0; j < bytes.length; j++)
+				blobBytes[j + totalBytesRead] = bytes[j];
+
+			startPointer = endPointer + 1;
+			totalBytesRead += bytes.length;
+		}
+
+		return blobBytes;
+	}
+
+	@Override
+	public String createUploadUrl(String url) {
+		return blobstoreService.createUploadUrl(url);
+	}
+
+}
---
+++ binary-storage/src/main/java/powered/by/sergey/binarystorage/BinaryStorageBOInFileImpl.java
@@ -0,0 +1,102 @@
+package powered.by.sergey.binarystorage;
+
+/*
+ * #%L
+ * ProjectX2013_03_23_web
+ * %%
+ * Copyright (C) 2013 - 2014 Powered by Sergey
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class BinaryStorageBOInFileImpl implements BinaryStorageBO {
+	private final String filePath;
+
+	BinaryStorageBOInFileImpl(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	@Override
+	public byte[] getFile(String id) {
+		File file = new File(filePath + id);
+		return file2Bytes(file);
+	}
+
+	@Override
+	public String saveFile(InputStream inputStream, HttpServletRequest req) {
+		String fileName = UUID.randomUUID().toString().replace("-", "");
+		String uploadedFileLocation = filePath + fileName;
+		writeToFile(inputStream, uploadedFileLocation);	
+		return fileName;
+	}
+
+	private byte[] file2Bytes(File file) {
+		byte[] bFile = new byte[(int) file.length()];
+		try {
+            //convert file into array of bytes
+			FileInputStream fileInputStream = new FileInputStream(file);
+		    fileInputStream.read(bFile);
+		    fileInputStream.close();
+		    return bFile;
+        } catch(Exception e) {
+        	e.printStackTrace();
+        }
+		return null;
+	}
+
+	private void writeToFile(InputStream uploadedInputStream,
+			String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void deleteFile(String id) {
+		File file = new File(filePath + id);
+		file.delete();
+	}
+
+	@Override
+	public String createUploadUrl(String url) {
+		return url;
+	}
+
+}
