a65f4de40bb8c9acaf946fe8f6c20b5f8e271150
Laesod <laesod@gmail.com>
2015-02-15 22:08:29 -0500

    Versin 0.0.4

---
+++ binary-storage/src/main/java/powered/by/sergey/binarystorage/FileNameHelper.java
@@ -0,0 +1,52 @@
+package powered.by.sergey.binarystorage;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/*
+ * #%L
+ * ProjectX2013_03_23_web
+ * %%
+ * Copyright (C) 2013 - 2015 Powered by Sergey
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+public class FileNameHelper {
+	
+	public String extractMediaType(String fileExtention) {		
+		return getPropertyValue(fileExtention.toLowerCase());
+	}
+	
+	private String getPropertyValue(String key) {
+		Properties prop = new Properties();
+		try {
+		    prop.load(FileNameHelper.class.getClassLoader().getResourceAsStream("media-type.properties"));
+		} 
+		catch (IOException ex) {
+		    ex.printStackTrace();
+		}
+		return prop.getProperty(key);
+	}
+
+	public String extractExtention(String fileName) {
+		int index = fileName.lastIndexOf(".");
+		if (index == -1) {
+			return null;
+		}
+		return fileName.substring(index, fileName.length()).toLowerCase();
+	}
+
+}
---
+++ binary-storage/src/test/java/powered/by/sergey/binarystorage/FileNameHelperTest.java
@@ -0,0 +1,63 @@
+package powered.by.sergey.binarystorage;
+
+/*
+ * #%L
+ * ProjectX2013_03_23_web
+ * %%
+ * Copyright (C) 2013 - 2015 Powered by Sergey
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import powered.by.sergey.binarystorage.FileNameHelper;
+
+public class FileNameHelperTest {
+	@Test 
+	public void extractExtention() {
+		FileNameHelper fnh = new FileNameHelper();
+		// positive
+		assertThat(fnh.extractExtention("Marusya.jpg"), is(".jpg"));
+		assertThat(fnh.extractExtention("Vasya.pdf"), is(".pdf"));
+		// negative
+		assertThat(fnh.extractExtention("Vasya"), is(nullValue()));
+		assertThat(fnh.extractExtention("jpg"), is(nullValue()));
+		assertThat(fnh.extractExtention("pdf"), is(nullValue()));
+		// antisocial
+		assertThat(fnh.extractExtention("Marusya.pdf.jpg"), is(".jpg"));
+		assertThat(fnh.extractExtention("Vasya.jpj.pdf"), is(".pdf"));
+		assertThat(fnh.extractExtention(""), is(nullValue()));
+		assertThat(fnh.extractExtention("Marusya."), is("."));
+
+	}
+	@Test
+	public void extractMediaType() {
+		FileNameHelper fnh = new FileNameHelper();
+		// positive
+		assertThat(fnh.extractMediaType(".jpg"), is("image/jpeg"));
+		assertThat(fnh.extractMediaType(".pdf"), is("application/pdf"));
+		assertThat(fnh.extractMediaType(".png"), is("image/jpeg"));		
+		// negative
+		assertThat(fnh.extractMediaType(".docx"), is(nullValue()));
+		// antisocial
+		assertThat(fnh.extractMediaType(""), is(nullValue()));
+		assertThat(fnh.extractMediaType("."), is(nullValue()));
+	}
+}
